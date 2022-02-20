a6e3af52ffa2e21e0a2e4a732c0c8ec1e2783898
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2022-02-20 23:55:19 +0700

    responsive signup login page

--- src/components/MainChat/ChatScreen.tsx
+++ src/components/MainChat/ChatScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
 import useGetUser from "../../hooks/useGetUser";
 import { ChatListItem } from "../../models/chat";
@@ -9,7 +9,7 @@ type ChatScreenProps = {};
 
 const ChatScreen = (props: ChatScreenProps) => {
   const user = useContext(ChatMainContext)?.content;
-
+  const heightRef = useRef<number>(0)
   useEffect(() => {
     const main = document.getElementById("chat__main");
     const list = document.getElementsByClassName(
@@ -17,14 +17,23 @@ const ChatScreen = (props: ChatScreenProps) => {
     ) as HTMLCollectionOf<HTMLElement>;
     if (list && list.length > 0) {
       const lastItem = list.item(list.length - 1);
-      if (lastItem) {
-        const height = lastItem.getBoundingClientRect().top;
-        main && main.scrollTo(0, height);
 
+      let height = 0;
+
+      if (lastItem) {
+        const itemHeight = lastItem.getBoundingClientRect().height;
+
+        for (let i = 0; i < list.length; i++) {
+            const item = list.item(i);
+            if (item) {
+                height += item.getBoundingClientRect().height;
+              }
+            heightRef.current = height+itemHeight
+            main && main.scrollTo(0, heightRef.current);
+        }
       }
     }
-    
-  }, [user]);
+  }, [user,heightRef.current]);
 
   return (
     <div className="chat__screen" id="chat__main">
